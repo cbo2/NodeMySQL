f8ceeda828620d27bf6fd72e8002b4d66b443e33
Craig Bomba <talk2cbo@gmail.com>
2018-08-16 15:41:04 -0500

    added a few things in prep to work on challenge 3 stuff

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table-redemption');
 var sqlpassword = "";
 var connection = {};
 
@@ -46,6 +47,23 @@ inquirer
 
 
         function queryUser() {
+
+            var table = new Table({
+                head: ['TH 1 label', 'TH 2 label'],
+                colWidths: [20, 30]
+            });
+             
+            // table is an Array, so you can `push`, `unshift`, `splice` and friends
+            table.push(
+                ['First value', 'Second value'],
+                ['First value', 'Second value']
+            );
+             
+            console.log(table.toString());
+            process.exit();
+
+
+
             connection.query("SELECT item_id, product_name, price, stock_quantity, product_sales from products", function (err, res) {
                 if (err) { throw err; }
                 // res.map(function (row) { console.log("==> item id is: " + row.item_id); });
@@ -95,35 +113,26 @@ inquirer
         // and populate it with some initial data
         function setupDB() {
 
-            connection.query("DROP DATABASE IF EXISTS bamazon", function (err, res) {
-                if (err) {
-                    console.log("There was an error dropping database bamazon with error: " + err);
-                    throw err;
-                }
-            });
-            connection.query("CREATE DATABASE bamazon", function (err, res) {
-                if (err) {
-                    console.log("There was an error dropping database bamazon with error: " + err);
-                    throw err;
-                }
-            });
             connection.query("USE bamazon", function (err, res) {
                 if (err) {
                     console.log("There was an error on the USE database bamazon with error: " + err);
                     throw err;
                 }
             });
-            var create = "CREATE TABLE products ("
-                + "item_id INTEGER(10) AUTO_INCREMENT NOT NULL,"
-                + "product_name VARCHAR(100) NOT NULL,"
-                + "department_name VARCHAR(40) NULL,"
-                + "price DECIMAL(10,2) NULL,"
-                + "stock_quantity INTEGER(10) NULL,"
-                + "product_sales DECIMAL(20,2) NULL,"           // this column added to satisfy challenge #3
-                + "PRIMARY KEY (item_id))";
+            connection.query("DROP TABLE IF EXISTS departments", function (err, res) {
+                if (err) {
+                    console.log("There was an error dropping database table departments with error: " + err);
+                    throw err;
+                }
+            });
+            var create = "CREATE TABLE departments ("
+            + "department_id INTEGER(10) AUTO_INCREMENT NOT NULL,"
+            + "department_name VARCHAR(40) NOT NULL,"
+            + "over_head_costs DECIMAL(20,2),"
+            + "PRIMARY KEY (department_id))"
             connection.query(create, function (err, res) {
                 if (err) {
-                    console.log("There was an error creating table products with error: " + err);
+                    console.log("There was an error creating table departments with error: " + err);
                     throw err;
                 }
                 seedDB();
@@ -132,23 +141,18 @@ inquirer
 
         function seedDB() {
             var items = [
-                { product_name: "Van Shoes", department_name: "Shoes", price: 50.25, stock_quantity: 333, product_sales: 2000.50 },
-                { product_name: "Seagate Ultra Thin USB 2TB Backup Drive", department_name: "Electronics", price: 99.99, stock_quantity: 5, product_sales: 2899.71 },
-                { product_name: "Seagate Ultra Thin USB 3TB Backup Drive", department_name: "Electronics", price: 149.99, stock_quantity: 100, product_sales: 7649.49 },
-                { product_name: "Seagate Ultra Thin USB 4TB Backup Drive", department_name: "Electronics", price: 199.99, stock_quantity: 200, product_sales: 199.99 },
-                { product_name: "Cuisinart 14 Cup Coffee Maker", department_name: "Housewares", price: 89.99, stock_quantity: 500, product_sales: 26997.00 },
-                { product_name: "Gothom Steel 12\" Fry Pan", department_name: "Housewares", price: 49.99, stock_quantity: 900, product_sales: 2499.50 },
-                { product_name: "Apple Macbook Pro 15\"", department_name: "Electronics", price: 2499.99, stock_quantity: 50, product_sales: 7499.97 },
-                { product_name: "Gillette Fusion Shaving Cream", department_name: "Bath and Body", price: 5.99, stock_quantity: 4999, product_sales: 5.99 },
-                { product_name: "Gillette Fusion 5 Shaver", department_name: "Bath and Body", price: 11.99, stock_quantity: 1000, product_sales: 0.00 },
-                { product_name: "Camelbak 25oz Sports Bottle", department_name: "Sports", price: 12.99, stock_quantity: 1000, product_sales: 12990.00 }
+                { department_name: "Shoes", over_head_costs: 20.00 },
+                { department_name: "Electronics", over_head_costs: 3333.33 },
+                { department_name: "Housewares", over_head_costs: 500.00 },
+                { department_name: "Bath and Body", over_head_costs: 988.12 },
+                { department_name: "Sports", over_head_costs: 129.90 }
             ]
-            var query = "INSERT INTO products set ?";
+            var query = "INSERT INTO departments set ?";
 
             items.map(function (item) {
                 connection.query(query, item, function (err, res) {
                     if (err) {
-                        console.log("There was an error on insert of item: " + items[i] + " and error: " + err);
+                        console.log("There was an error on insert of item: " + item + " and error: " + err);
                         throw err;
                     }
                     // console.log("Item inserted: " + item.product_name);
